6410a34d99590013be8b7f352d83cb985d2dd580
Matúš Csirik <matus.csirik@gmail.com>
2025-12-08 20:14:01 +0100

    feat: nicer header type

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,7 @@ import { getAccessToken } from "../features/auth/model/pkce";
 
 export const CLIENT_ID = "itu";
 export const REDIRECT_URI = "http://localhost:3000/auth/callback";
-export const SCOPES = "board:play challenge:write";
+export const SCOPES = "board:play challenge:write challenge:read";
 
 export function createAuthHeaders(accept?: string): RequestInit {
 	const token = getAccessToken();
@@ -13,7 +13,7 @@ export function createAuthHeaders(accept?: string): RequestInit {
 		throw new Error("No access token available. User must be authenticated.");
 	}
 
-	const headers: Record<string, string> = {
+	const headers: HeadersInit = {
 		Authorization: `Bearer ${token}`,
 	};
 
