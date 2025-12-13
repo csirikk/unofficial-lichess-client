9f3d618b5f3493cdfa56557660dc84344a5496bd
Matúš Csirik <matus.csirik@gmail.com>
2025-12-13 22:36:25 +0100

    fix: remove scrollbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,7 +13,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
 	return (
-		<div className="flex md:overflow-auto h-screen flex-col overflow-hidden cursor-default bg-[rgb(var(--color-surface-base))] text-[rgb(var(--color-fg-primary))]">
+		<div className="flex overflow-auto lg:overflow-hidden h-screen flex-col cursor-default bg-[rgb(var(--color-surface-base))] text-[rgb(var(--color-fg-primary))]">
 			<Navbar />
 			<main className="flex flex-1 items-stretch justify-center px-4 py-2">
 				<div className="flex h-full min-h-0">{children}</div>
